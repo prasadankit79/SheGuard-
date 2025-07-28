296a2f6575661345e566dada7bc9a6d35a26648f
prasadankit79 <ankitprasad797979@gmail.com>
2025-07-29 00:10:55 +0530

    Initial commit of SheGuard+ app

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -3,7 +3,6 @@ import { getFirestore } from 'firebase/firestore';
 import { initializeAuth, getReactNativePersistence } from 'firebase/auth';
 import ReactNativeAsyncStorage from '@react-native-async-storage/async-storage';
 
-// Import the keys securely from the .env file
 import { 
     FIREBASE_API_KEY, 
     FIREBASE_AUTH_DOMAIN, 
@@ -13,7 +12,6 @@ import {
     FIREBASE_APP_ID 
 } from '@env';
 
-// Use the imported keys to build the config object
 const firebaseConfig = {
   apiKey: FIREBASE_API_KEY,
   authDomain: FIREBASE_AUTH_DOMAIN,
@@ -23,16 +21,13 @@ const firebaseConfig = {
   appId: FIREBASE_APP_ID
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 
-// Initialize Auth with persistence
 const auth = initializeAuth(app, {
   persistence: getReactNativePersistence(ReactNativeAsyncStorage)
 });
 
-// This is a unique ID for the app instance, provided by the environment.
 const appId = typeof __app_id !== 'undefined' ? __app_id : 'default-sheguard-app';
 
 export { auth, db, appId };
